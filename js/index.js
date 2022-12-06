b02482423ff87a71ce1c90a66caeff3ad5dc5d57
slackku <bananuya3@gmail.com>
2022-12-05 23:05:41 -0300

    Cambios menores en la maqueta

--- js/index.js
+++ js/index.js
@@ -1,18 +1,13 @@
 window.addEventListener("resize", function () {
-  var image = document.querySelector(".logardo");
-  if (window.innerWidth < 600) {
-    image.src = "img/asdmini.png";
-  }
   let text = document.querySelectorAll(".btn-group .btn");
-  if (this.window.innerWidth < 520) {
+  if (this.window.innerWidth < 532) {
     text[0].innerHTML = "Intereses";
     text[1].innerHTML = "Añadir";
     text[2].innerHTML = "Más";
   }
-  if (this.window.innerWidth > 520) {
+  if (this.window.innerWidth > 532) {
     text[0].innerHTML = "Tengo interes en";
     text[1].innerHTML = "Añadir Seleccion";
     text[2].innerHTML = "Más";
-    image.src = "img/asd.png";
   }
 });
