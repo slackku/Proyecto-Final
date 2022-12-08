1edd9ff33523e8d2b482b9f279718d79bf149d3e
slacku <bananuya3@gmail.com>
2022-12-08 00:36:01 -0300

    Integracion de Angular a la maqueta

---
+++ PortfolioFrontEnd/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { MainInfoComponent } from './components/main-info/main-info.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    MainInfoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ PortfolioFrontEnd/src/app/components/main-info/main-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-info',
+  templateUrl: './main-info.component.html',
+  styleUrls: ['./main-info.component.css']
+})
+export class MainInfoComponent {
+
+}
