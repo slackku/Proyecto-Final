681c48b335da28238d697b610a0bd47b8303ec26
slacku <bananuya3@gmail.com>
2023-04-12 01:28:27 -0300

    Deletion reference of no more existing components, added login component and ext.info, plus HTTPClientModule

--- PortfolioFrontEnd/src/app/app.module.ts
+++ PortfolioFrontEnd/src/app/app.module.ts
@@ -6,31 +6,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { MainInfoComponent } from './components/main-info/main-info.component';
-import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
-import { StyleBtnComponent } from './components/style-btn/style-btn.component';
 import { CursorComponent } from './components/cursor/cursor.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { StylingToolComponent } from './components/styling-tool/styling-tool.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ExtendedInfoComponent } from './components/extended-info/extended-info.component';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
     MainInfoComponent,
-    AcercaDeComponent,
-    ExperienciaComponent,
-    EducacionComponent,
-    StyleBtnComponent,
     CursorComponent,
     LogoutComponent,
     StylingToolComponent,
+    ExtendedInfoComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
