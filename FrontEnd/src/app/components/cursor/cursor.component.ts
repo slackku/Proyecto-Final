e0c9a526d8d445e6ebddc145a227f492460395ce
slacku <bananuya3@gmail.com>
2023-04-25 12:28:15 -0300

    Smoothness of the cursor

--- FrontEnd/src/app/components/cursor/cursor.component.ts
+++ FrontEnd/src/app/components/cursor/cursor.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-cursor',
@@ -7,6 +6,7 @@ import { timeout } from 'rxjs';
   styleUrls: ['./cursor.component.css'],
 })
 export class CursorComponent {
+
   ngOnInit(): void {
     const cursor = document.querySelector('#cursor') as HTMLElement;
     document.addEventListener('mousemove', (e) => {
@@ -21,11 +21,10 @@ export class CursorComponent {
     const links = document.querySelector('a') as HTMLElement;
     // Correccion para el hover de botones log in & out
     // *ngIf directamente en los html o en los contenedores generados de Angular
-    // no permite la correcta obtencion de dichos elementos con metodos de java 
+    // no permite la correcta obtencion de dichos elementos con metodos de java
     //(retornan listas de lenght 0)
-    const btns = document.getElementsByClassName('button-container'); 
+    const btns = document.getElementsByClassName('button-container');
     const colorSph = document.getElementsByClassName('colorSph');
-
     this.eloHtmlColection(colorSph, cursor);
     this.eloHtmlColection(btns_edit, cursor);
     this.eloHtmlColection(btns, cursor);
@@ -34,14 +33,16 @@ export class CursorComponent {
 
   eloHtmlElement(htmlE: HTMLElement, cursor: HTMLElement) {
     //Event Listener on a HTMLElement
-    htmlE.addEventListener('mouseenter', () => {
-      cursor.classList.toggle('normal');
-      cursor.classList.add('ontext');
-    });
-    htmlE.addEventListener('mouseleave', () => {
-      cursor.classList.toggle('ontext');
-      cursor.classList.add('normal');
-    });
+    if (htmlE != null) {
+      htmlE.addEventListener('mouseenter', () => {
+        cursor.classList.toggle('normal');
+        cursor.classList.add('ontext');
+      });
+      htmlE.addEventListener('mouseleave', () => {
+        cursor.classList.toggle('ontext');
+        cursor.classList.add('normal');
+      });
+    }
   }
   eloHtmlColection(htmlC: HTMLCollection, cursor: HTMLElement) {
     //Event Listener on a HTMLCollection
