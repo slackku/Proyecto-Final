6b38545b8a64274d00e91811373867bc37f1c901
slacku <bananuya3@gmail.com>
2023-05-08 02:32:58 -0300

    ngIf added to the component, based on localStorage

--- FrontEnd/src/app/components/logout/logout.component.ts
+++ FrontEnd/src/app/components/logout/logout.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent {
-  isSessionOn : boolean = false;
+  isSessionOn = parseInt(
+    localStorage.getItem('isSessionOn')?.toString() as string
+  );
+  constructor(private router: Router) {}
+
+  logOut() {
+    localStorage.setItem('isSessionOn', '0');
+    location.reload();
+  }
 }
